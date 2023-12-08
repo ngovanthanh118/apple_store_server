d5c1270b79d4519d03abd9d86976c0f992e71eab
ngovanthanh118 <nvthanh118@gmail.com>
2023-12-08 17:22:35 +0700

    fix deploy

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+const Product = require('../models/productSchema');
+const Comment = require('../models/commentSchema');
+const User = require('../models/userSchema');
 
 const connect = async () => {
     try {
         await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(() => console.log('Connect to database successfully!'))
             .catch(() => console.log('Connect to database failure!'))
+        await Product;
+        await Comment;
+        await User;
     }
     catch (err) {
         handleError(err);
