c57a0a6c96fdb25170a5a5761ebd627c9fcb09ea
ngovanthanh118 <nvthanh118@gmail.com>
2023-12-25 13:24:42 +0700

    fix cookies

--- src/untils/auth.js
+++ src/untils/auth.js
@@ -4,7 +4,7 @@ class Authentication {
     //check login
     async checkLogin(req, res, next) {
         try {
-            const token = req.cookies.token;
+            const token = req.headers.token;
             const _id = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
             if (_id) {
                 await User.findOne({ _id: _id })
